fdc90db7b834090fb7e70802c14f890a7bc3ba88
Dan Peirce <peirce.dan@gmail.com>
2023-07-09 13:51:40 -0700

    added webmanager

--- ESP32_SDWebServer.ino
+++ ESP32_SDWebServer.ino
@@ -35,6 +35,7 @@
 #include <ESPmDNS.h>
 #include <SPI.h>
 #include <SD.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 #define DBG_OUTPUT_PORT Serial
 
@@ -267,22 +268,33 @@ void setup(void) {
   DBG_OUTPUT_PORT.setDebugOutput(true);
   DBG_OUTPUT_PORT.print("\n");
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  DBG_OUTPUT_PORT.print("Connecting to ");
-  DBG_OUTPUT_PORT.println(ssid);
+//WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
 
-  // Wait for connection
-  uint8_t i = 0;
-  while (WiFi.status() != WL_CONNECTED && i++ < 20) {//wait 10 seconds
-    delay(500);
-  }
-  if (i == 21) {
-    DBG_OUTPUT_PORT.print("Could not connect to");
-    DBG_OUTPUT_PORT.println(ssid);
-    while (1) {
-      delay(500);
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    // wm.resetSettings();
+
+        // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("AutoConnectAP","physicsPMT"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected... ");
     }
-  }
+    Serial.println();
+    Serial.println();
   DBG_OUTPUT_PORT.print("Connected! IP address: ");
   DBG_OUTPUT_PORT.println(WiFi.localIP());
 
